fab2ef665bedf674734bc4c52aca46e56a6c7ef4
xyh <474318838@qq.com>
2016-12-08 22:46:14 +0800

    update

--- algorithms/graph/graph.java
+++ algorithms/graph/graph.java
@@ -39,3 +39,80 @@ public class Graph
 
 	public Iterable<Integer> adj(int v) { return adj[v]; }
 }
+
+
+// 深度优先搜素
+public class DepthFirstSearch
+{
+	private boolean[] marked;  // v和s是连通的吗
+	private int count;				// 与s连通的顶点总数
+
+	public DepthFirstSearch(Graph G, int s)
+	{
+		marked = new boolean[G.V()];
+		dfs(G, s);
+	}
+
+	private void dfs(Graph G, int v)
+	{
+		marked[v] = true;
+		count++;
+		for(int w : G.adj(v))
+			if(!marked[w]) dfs(G, w);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+	}
+
+	public boolean marked(int w)
+	{ return marked[w]; }
+
+	public int count()
+	{ return count; }
+}
+
+
+// 使用深度优先搜索查找图中的路径
+public class DepthFirstPaths
+{
+	private boolean[] marked;		//这个顶点上调用过dfs()了吗？
+	private int[] edgeTo;				//从起点到一个顶点的已知路径上的最后一个顶点
+	private final int s;			// 起点
+
+	public DepthFirstPaths(Graph G, int s)
+	{
+		marked = new boolean[G.V()];
+		edgeTo = new int[G.V()];
+		this.s = s;
+		dfs(G, s);
+	}
+
+	private void dfs(Graph G, int v)
+	{
+		marked[v] = true;
+		for(int w : G.adj(v))
+			if (!marked[w])
+			{
+				edgeTo[w] = v;
+				dfs(G, w);	
+			}
+	}
+
+	public boolean hasPathTo(int v)
+	{ return marked[V]; }
+
+	public Iterable<Integer> pathTo(int v)
+	{
+		if(!hasPathTo(v)) return null;
+		Stack<Integer> path = new Stack<Integer>();
+		for(int x = v; x != s; x = edgeTo[x])
+			path.push(x);
+		path.push(s);
+		return path;
+	}
+}
+
+
+
+// 使用广度优先搜索查找图中的路径
+
+
+
+
