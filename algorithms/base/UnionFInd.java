101821a016d98cbae09d10eb1bbb23dd76718782
xyh <474318838@qq.com>
2017-01-07 11:30:39 +0800

    add weighted quick union algorithm

--- algorithms/base/UnionFInd.java
+++ algorithms/base/UnionFInd.java
@@ -64,7 +64,7 @@ public void union(int p, int q)
 // --- 拓展----- quick-union算法
 private int find(int p)
 {	// 找出分量的名称
-	while(p != id[p])	p = id[p];
+	while(p != id[p])	p = id[p];	// 只有根结点 p = id[p];
 	return p;
 }
 public void union(int p, int q)
@@ -80,5 +80,44 @@ public void union(int p, int q)
 
 
 
+// union-find算法的实现（加权quick-union算法）
+public class WeightedQuickUnionUF
+{
+	private int[] id;		// 父链接的数组（由触点索引）
+	private int[] sz;		// (由触点索引的)各个根节点所对应的分量的大小
+	private int count;		// 连通分量的数量
 
+	public WeightedQuickUnionUF(int N)
+	{
+		count = N;
+		id = new int[N];
+		for(int i = 0; i < N; i++) id[i] = i;
+		sz = new int[N];
+		for(int i = 0; i < N; i++)	sz[i] = 1;
+	}
+	
+	public int count()
+	{ return count; }
+
+	public boolean connected(int p, int q)
+	{ return find(p) == find(q); }
+
+	public int find(int p)
+	{	// 跟随链接找到根结点
+		while(p != id[p])	p = id[p];	// 
+		return p;
+	}
+
+	public void union(int p, int q)
+	{
+		int i = find(p);
+		int j = find(q);
+		if(i == j) return;
+
+		// 将小树的根节点连接到大树的根节点
+		if 		(sz[i] < sz[j])	{ id[i] = j; sz[j] += sz[i]; }
+		else 					{ id[j] = i; sz[i] += sz[j]; }
+		count--;
+	}
+}
 
